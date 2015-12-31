5aeba6c2c68a5e19419ef2d4aa1dd60773d50a26
Jun Takahashi <juntaro123@gmail.com>
2016-01-01 06:49:01 +0900

    3rd

--- add.php
+++ add.php
@@ -7,18 +7,18 @@ $category = (int) $_POST['category'];
 $difficulty = (int) $_POST['difficulty'];
 $budget = (int) $_POST['budget'];
 try {
-    $dbh = new PDO('sqlsrv:server=.\sqlexpress;database=db1', $user, $pass);
-    $dbh->setAttribute(PDO::ATTR_EMULATE_PREPARES, FALSE);
-    $dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+	$dbh = new PDO('sqlsrv:server=.\sqlexpress;database=db1', $user, $pass);
+	$dbh->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
+	$dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     $sql = "INSERT INTO recipes (recipe_name, category, difficulty, budget, howto) VALUES (?, ?, ?, ?, ?)";
     $stmt = $dbh->prepare($sql);
-    $stmt->bindvalue(1, $recipe_name, PDO::PARAM_STR);
-    $stmt->bindvalue(2, $category,  PDO::PARAM_INT);
-    $stmt->bindvalue(3, $difficulty,  PDO::PARAM_INT);
-    $stmt->bindvalue(4, $budget, PDO::PARAM_INT);
-    $stmt->bindvalue(5, $howto,  PDO::PARAM_STR);
-    $stmt = execute();
-    $dbh = null;
+    $stmt->bindValue(1, $recipe_name, PDO::PARAM_STR);
+	$stmt->bindValue(2, $category, PDO::PARAM_INT);
+	$stmt->bindValue(3, $difficulty, PDO::PARAM_INT);
+	$stmt->bindValue(4, $budget, PDO::PARAM_INT);
+	$stmt->bindValue(5, $howto, PDO::PARAM_STR);
+    $stmt->execute();
+	$dbh = null;
     echo "レシピの登録が完了しました。";
 }
 catch (PDOException $e){
