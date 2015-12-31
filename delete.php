72ed763fbaf7f53393d5fc772063bfe6f17c6857
Jun Takahashi <juntaro123@gmail.com>
2016-01-01 08:19:46 +0900

    4th

---
+++ delete.php
@@ -0,0 +1,21 @@
+<?php
+$user = "sa";
+$pass = "jtakaha4";
+try {
+    if (empty($_GET['id'])) throw new Exception('Error');
+    $id = (int) $_GET['id'];
+    $dbh = new PDO('sqlsrv:server=.\sqlexpress;database=db1', $user, $pass);
+    $dbh->setAttribute(PDO::ATTR_EMULATE_PREPARES, FALSE);
+    $dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    $sql = "DELETE FROM recipes WHERE id = ?";
+    $stmt = $dbh->prepare($sql);
+    $stmt->bindValue(1, $id, PDO::PARAM_INT);
+    $stmt->execute();
+    $dbh = null;
+    echo "ID： " . htmlspecialchars($id, ENT_QUOTES, 'UTF-8') . "の削除が完了しました。";
+}
+catch (Exception $e) {
+    echo "エラー発生: " . htmlspecialchars($e->getMessage(), ENT_QUOTES, 'UTF-8') . "<br>";
+    die();
+}
+?>
